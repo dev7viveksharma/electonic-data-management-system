890accd389dd8db9b06880e69ac8f204bd1f837a
dev7viveksharma <viveklvs123@gmail.com>
2025-04-18 14:26:34 +0530

    alignment fixing for big screen

--- edms.js
+++ edms.js
@@ -4,6 +4,7 @@ class LOGIN{
         this.login = document.querySelector(".l_main");
         this.signup = document.querySelector(".signup_screen");
         this.backbtn = document.querySelector(".js_back");
+        this.body = document.querySelector("body");
         this.init();
     }
 
@@ -15,11 +16,13 @@ class LOGIN{
     signuppage(event){
         this.login.classList.add("hidden");
         this.signup.classList.remove("hidden");
+        this.body.classList.add("body_s");
     }
 
     backtologin(event){
         this.signup.classList.add("hidden");
         this.login.classList.remove("hidden");
+        this.body.classList.remove("body_s");
     }
 }
 
