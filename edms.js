54242f7dba8e154306ae9c836ef86e80db648568
dev7viveksharma <viveklvs123@gmail.com>
2025-04-19 23:41:15 +0530

    login complete

--- edms.js
+++ edms.js
@@ -1,4 +1,4 @@
-class LOGIN{
+class LOGIN {
     constructor(){
         this.noAccount = document.querySelector(".toggle");
         this.login = document.querySelector(".l_main");
@@ -14,9 +14,10 @@ class LOGIN{
     }
 
     init(){
-        this.noAccount.addEventListener("click",(event)=>this.signuppage(event));
-        this.backbtn.addEventListener("click",(event)=>this.backtologin(event));
-        this.login_btn.addEventListener("click",(event)=>this.signupcheck(event));
+        this.noAccount.addEventListener("click", (event) => this.signuppage(event));
+        this.backbtn.addEventListener("click", (event) => this.backtologin(event));
+        this.login_btn.addEventListener("click", (event) => this.signupcheck(event));
+
     }
 
     signuppage(){
@@ -33,26 +34,135 @@ class LOGIN{
 
     signupcheck(event){
         event.preventDefault();
-        if(this.login_username.value.trim() === "" && this.login_password.value === "" && this.login_emp.value === ""){
+        if (this.login_username.value.trim() === "" && this.login_password.value === "" && this.login_emp.value === "") {
             this.errorlogin("Please Enter Valid Details");
-
+        } else if (this.login_username.value.trim() === "" && this.login_password.value !== "" && this.login_emp.value !== "") {
+            this.errorusername("please enter your name");
+        } else if (this.login_username.value !== "" && this.login_password.value === "" && this.login_emp.value !== "") {
+            this.errorpassword("please enter your correct password");
+        } else if (this.login_username.value !== "" && this.login_password.value !== "" && this.login_emp.value === "") {
+            this.erroremp("please enter your valid employee code");        
+        } else {
+            if (this.login_password !== "") {
+                this.validpassword(this.login_password);
+            }
+            if(this.login_emp !== ""){
+                this.validcode(this.login_emp);
+            }
         }
     }
 
     errorlogin(message){
         const exist = document.querySelector("#loginError");
-        if(exist){
-            return;
+        if (exist) {
+            exist.remove();
         }
         const error = document.createElement("p");
         error.id = "loginError";
         error.textContent = message;
         error.style.color = "red";
         error.style.margin = "0";
-        error.style.marginTop = "10px";
+        error.style.marginTop = "5px";
+        error.style.fontSize = "0.8rem";
 
         this.login_password.insertAdjacentElement("afterend", error);
-      }
+    }
+
+    errorusername(message){
+        const exist = document.querySelector("#loginError");
+        if (exist) {
+            exist.remove();
+        }
+        const error = document.createElement("p");
+        error.id = "loginError";
+        error.textContent = message;
+        error.style.color = "red";
+        error.style.margin = "0";
+        error.style.marginTop = "5px";
+        error.style.fontSize = "0.8rem";
+
+        this.login_password.insertAdjacentElement("afterend", error);
+    }
+
+    errorpassword(message){
+        const exist = document.querySelector("#loginError");
+        if (exist) {
+            exist.remove();
+        }
+        const error = document.createElement("p");
+        error.id = "loginError";
+        error.textContent = message;
+        error.style.color = "red";
+        error.style.margin = "0";
+        error.style.marginTop = "5px";
+        error.style.fontSize = "0.8rem";
+
+        this.login_password.insertAdjacentElement("afterend", error);
+    }
+
+    erroremp(message){
+        const exist = document.querySelector("#loginError");
+        if (exist) {
+            exist.remove();
+        }
+        const error = document.createElement("p");
+        error.id = "loginError";
+        error.textContent = message;
+        error.style.color = "red";
+        error.style.margin = "0";
+        error.style.marginTop = "5px";
+        error.style.fontSize = "0.8rem";
+
+        this.login_emp.insertAdjacentElement("afterend", error);
+    }
+
+    validpassword(login_password){
+        const password = login_password.value;
+        const conditions = [
+            { test: password.length >= 8, message: "atleast 8 character long" },
+            { test: /[A-Z]/.test(password), message: "atleast 1 Uppercase letter [A-Z]" },
+            { test: /[a-z]/.test(password), message: "atleast 1 Lowercase letter [a-z]" },
+            { test: /[0-9]/.test(password), message: "atleast 1 NUMBER [0-9]" },
+            { test: /[!@#$%^&*?]/.test(password), message: "atleast 1 Special Character [!@#$%^&*?]" }
+        ];
+
+        conditions.forEach(conditions => {
+            if (!conditions.test) {
+                const exist = document.querySelector("#loginError");
+                if (exist) {
+                    exist.remove();
+                }
+                const error = document.createElement("p");
+                error.id = "loginError";
+                error.textContent = conditions.message;
+                error.style.color = "red";
+                error.style.margin = "0";
+                error.style.marginTop = "5px";
+                error.style.fontSize = "0.8rem";
+
+                this.login_password.insertAdjacentElement("afterend", error);
+            }
+        });
+    }
+
+    validcode(code){
+        const empcode = code.value;
+        if(empcode.length !== 6){
+            const exist = document.querySelector("#loginError");
+            if (exist) {
+                exist.remove();
+            }
+            const error = document.createElement("p");
+            error.id = "loginError";
+            error.textContent = "employee code is not 6 digit long";
+            error.style.color = "red";
+            error.style.margin = "0";
+            error.style.marginTop = "5px";
+            error.style.fontSize = "0.8rem";
+
+            this.login_emp.insertAdjacentElement("afterend", error);
+        } 
+    }
 }
 
 new LOGIN();
